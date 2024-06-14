da7e3e0988045e206d44efcc2d7a397acc244e46
Moraj <109418239+morajdhwaj@users.noreply.github.com>
2024-06-14 13:50:55 +0530

    [INIT] video chat app

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import { Typography, AppBar } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { Typography, AppBar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-import VideoPlayer from './components/VideoPlayer';
-import Sidebar from './components/Sidebar';
-import Notifications from './components/Notifications';
+import VideoPlayer from "./components/VideoPlayer";
+import Sidebar from "./components/Sidebar";
+import Notifications from "./components/Notifications";
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
     borderRadius: 15,
-    margin: '30px 100px',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '600px',
-    border: '2px solid black',
+    margin: "30px 100px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "600px",
+    border: "2px solid black",
 
-    [theme.breakpoints.down('xs')]: {
-      width: '90%',
+    [theme.breakpoints.down("xs")]: {
+      width: "90%",
     },
   },
   image: {
-    marginLeft: '15px',
+    marginLeft: "15px",
   },
   wrapper: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    width: '100%',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
   },
 }));
 
@@ -38,7 +38,9 @@ const App = () => {
   return (
     <div className={classes.wrapper}>
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography variant="h2" align="center">Video Chat</Typography>
+        <Typography variant="h2" align="center">
+          Video Chat
+        </Typography>
       </AppBar>
       <VideoPlayer />
       <Sidebar>
